f7f8aa309f3bb98f297aec143876cb20b0a7c2e5
Elton Renda <erenda@equalexperts.com>
2021-08-12 19:45:27 +0200

    fix: npm verion updates

--- lxc/nginx-proxy-manager/install/alpine.sh
+++ lxc/nginx-proxy-manager/install/alpine.sh
@@ -118,8 +118,8 @@ ln -sf /usr/local/openresty/nginx/sbin/nginx /usr/sbin/nginx
 ln -sf /usr/local/openresty/nginx/ /etc/nginx
 
 # Update NPM version in package.json files
-sed -i "s+0.0.0+#$_latest_version+g" backend/package.json
-sed -i "s+0.0.0+#$_latest_version+g" frontend/package.json
+sed -i "s+0.0.0+$_latest_version+g" backend/package.json
+sed -i "s+0.0.0+$_latest_version+g" frontend/package.json
 
 # Fix nginx config files for use with openresty defaults
 sed -i 's+^daemon+#daemon+g' docker/rootfs/etc/nginx/nginx.conf
