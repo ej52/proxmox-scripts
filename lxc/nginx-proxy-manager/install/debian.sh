ba279364e5fd6b6e7caefeb6c8f40a0551e789ef
Alexis <45032998+Alexis-BX@users.noreply.github.com>
2023-06-13 00:40:45 -0400

    added fix to nginx.conf

--- lxc/nginx-proxy-manager/install/debian.sh
+++ lxc/nginx-proxy-manager/install/debian.sh
@@ -208,6 +208,9 @@ cd /app
 export NODE_ENV=development
 runcmd yarn install --network-timeout=30000
 
+sudo sed -i 's/^pid/# pid/' /usr/local/openresty/nginx/conf/nginx.conf
+sudo sed -i 's/^user npm/user root/' /usr/local/openresty/nginx/conf/nginx.conf
+
 # Create NPM service
 log "Creating NPM service"
 cat << 'EOF' > /lib/systemd/system/npm.service
