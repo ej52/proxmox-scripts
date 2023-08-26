8963a735f93f570a8fbbb57b06cce9cdf5cfff30
Ronny Roethof <ronny@roethof.net>
2023-08-26 08:57:14 +0200

    Update alpine.sh
    Added py-pip3 for compatibility with current alpine versions

--- lxc/nginx-proxy-manager/install/alpine.sh
+++ lxc/nginx-proxy-manager/install/alpine.sh
@@ -89,7 +89,7 @@ runcmd apk upgrade
 
 # Install dependancies
 log "Installing dependencies"
-runcmd 'apk add python3 openresty nodejs yarn openssl apache2-utils logrotate $DEVDEPS'
+runcmd 'apk add python3 openresty nodejs yarn openssl apache2-utils logrotate py3-pip $DEVDEPS'
 
 # Setup python env and PIP
 log "Setting up python"
