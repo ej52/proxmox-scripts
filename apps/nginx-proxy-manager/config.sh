9331782c5aa6468eb9eadc40f3ce39dceabf95aa
craeckor <52975262+craeckor@users.noreply.github.com>
2024-02-11 18:19:31 +0100

    bump rust version
    bump rust version to 1.76.0

--- apps/nginx-proxy-manager/config.sh
+++ apps/nginx-proxy-manager/config.sh
@@ -22,7 +22,7 @@ EPS_CT_SWAP=${EPS_CT_SWAP:-0}
 # NPM package versions
 NODE_VERSION="v16.20.2"
 YARN_VERSION="1.22.19"
-RUST_VERSION="1.74.0"
+RUST_VERSION="1.76.0"
 
 EPS_SERVICE_FILE=/etc/init.d/npm
 EPS_SERVICE_DATA="#!/sbin/openrc-run\ndescription=\"Nginx Proxy Manager\"\n\ncommand=\"/usr/local/bin/node\"\ncommand_args=\"index.js --abort_on_uncaught_exception --max_old_space_size=250\"\ncommand_background=\"yes\"\ndirectory=\"/app\"\n\npidfile=\"/var/run/npm.pid\"\noutput_log=\"/var/log/npm.log\"\nerror_log=\"/var/log/npm.err\"\n\ndepends () {\n  before openresty\n}\n\nstart_pre() {\n  mkdir -p /tmp/nginx/body /data/letsencrypt-acme-challenge\n  export NODE_ENV=production\n}\n\nstop() {\n  pkill -9 -f node\n  return 0\n}\n\nrestart() {\n  \$0 stop\n  \$0 start\n}"
